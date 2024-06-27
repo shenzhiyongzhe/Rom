530d65bb228d16bf09f18113116a27a3447db0bb
Mei_X <3183727659@qq.com>
2024-06-27 14:08:34 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,24 @@
+// "ui";
+const { baseUrl } = require("./Color.js");
+const { Sleep, ReadImg, RandomPress, NumberRecognition, GetCurrentDate } = require("./Utils.js");
+
+// "nodejs";
+// class Test
+// {
+//     constructor()
+//     {
+//         this.test = 1;
+//     }
+//     test()
+//     {
+//         console.log("test");
+//     }
+// }
+// const a = new Test();
+// a.test();
+// let a;
+// for (let i = 0; i < 5; i++)
+// {
+//     a = i;
+//     console.log(a);
+// }
